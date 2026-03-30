50210189f473c9f69166562b77f41248e20404c2
HarsJaip <jaipuriaharshit146@gmail.com>
2026-03-30 19:43:10 +0530

    updated the skills section

--- src/components/about/Skills.jsx
+++ src/components/about/Skills.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 const skillsContent = [
-  { skillClass: "p25", skillPercent: "25", skillName: "HTML" },
-  { skillClass: "p89", skillPercent: "89", skillName: "JAVASCRIPT" },
-  { skillClass: "p70", skillPercent: "70", skillName: "CSS" },
-  { skillClass: "p66", skillPercent: "66", skillName: "PHP" },
-  { skillClass: "p95", skillPercent: "95", skillName: "WORDPRESS" },
-  { skillClass: "p50", skillPercent: "50", skillName: "JQUERY" },
-  { skillClass: "p65", skillPercent: "65", skillName: "ANGULAR" },
-  { skillClass: "p45", skillPercent: "45", skillName: "REACT" },
+  { skillClass: "p70", skillPercent: "70", skillName: "HTML/CSS" },
+  { skillClass: "p50", skillPercent: "50", skillName: "JAVASCRIPT/TypeScript" },
+  { skillClass: "p70", skillPercent: "70", skillName: "JAVA/C" },
+  { skillClass: "p30", skillPercent: "30", skillName: "Python" },
+  { skillClass: "p70", skillPercent: "70", skillName: "Next.js" },
+  { skillClass: "p50", skillPercent: "50", skillName: "Node.js" },
+  { skillClass: "p65", skillPercent: "65", skillName: "React / Tailwind css" },
+  { skillClass: "p45", skillPercent: "45", skillName: "MySQL / MongoDB" },
 ];
 
 const Skills = () => {

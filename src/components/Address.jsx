c53c682015d7ec21569d45d3fd4cc2dc6f780b37
HarsJaip <jaipuriaharshit146@gmail.com>
2026-04-08 21:46:49 +0530

    updated the personal details

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -5,22 +5,21 @@ const Address = () => {
     <>
       <p className="open-sans-font custom-span-contact position-relative">
         <i className="fa fa-map position-absolute"></i>
-        <span className="d-block">Address Point</span>123 Stree New York City ,
-        United States Of America 750065.
+        <span className="d-block">Address Point</span> D-15/13 Man Mandir Dashashwamedh Varanasi 221001
       </p>
       {/* End .custom-span-contact */}
 
       <p className="open-sans-font custom-span-contact position-relative">
         <i className="fa fa-envelope-open position-absolute"></i>
         <span className="d-block">mail me</span>{" "}
-        <a href="mailto:steve@mail.com">steve@mail.com</a>
+        <a href="mailto:jaipuriaharshit146@gmail.com">jaipuriaharshit146@gmail.com</a>
       </p>
       {/* End .custom-span-contact */}
 
       <p className="open-sans-font custom-span-contact position-relative">
         <i className="fa fa-phone-square position-absolute"></i>
         <span className="d-block">call me</span>{" "}
-        <a href="Tel: +216 21 184 010">+216 21 184 010</a>
+        <a href="Tel: +91 8318144369">+91 8318144369</a>
       </p>
       {/* End .custom-span-contact */}
     </>

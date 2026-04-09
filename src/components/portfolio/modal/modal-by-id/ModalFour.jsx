5c94470a8e1fdeafe735b7e4d33f6065ffded69f
HarsJaip <jaipuriaharshit146@gmail.com>
2026-04-09 23:08:02 +0530

    updated the modelfour for better rendering

--- src/components/portfolio/modal/modal-by-id/ModalFour.jsx
+++ src/components/portfolio/modal/modal-by-id/ModalFour.jsx
@@ -1,86 +1,103 @@
+// external
 import Image from 'next/image';
 import React from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+// internal
 import CloseImg from "../../../../../public/assets/img/cancel.svg";
 import PortfolioData from '../../portfolioData';
+import img1 from "../../../../../public/assets/img/portfolio/project4/1.jpg";
+import img2 from "../../../../../public/assets/img/portfolio/project4/2.jpg";
+import img3 from "../../../../../public/assets/img/portfolio/project4/3.jpg";
 
 const ModalFour = ({ modalId, setGetModal }) => {
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+  };
   return (
     <div className="modal_portfolio">
-      <div
-        className="modal__outside"
-        onClick={() => setGetModal(false)}
-      ></div>
-      <div className="modal__content">
-        {PortfolioData.filter((item) => item.id === modalId).map((item) => {
-          //
-          return (
-            <div key={item.id} data-aos="fade">
-              <h2 className="heading mb-2">{item.type}</h2>
-              <div className="modal__details">
-                {item.modalDetails.map((details, i) => {
-                  return (
-                    <div key={i} className="row open-sans-font">
-                      <div className="col-12 col-sm-6 mb-2">
-                        <i className="fa fa-file-text-o pr-2"></i>
-                        Project:{" "}
-                        <span className="ft-wt-600 uppercase">
-                          {details.project}
-                        </span>
+        <div
+          className="modal__outside"
+          onClick={() => setGetModal(false)}
+        ></div>
+        <div className="modal__content">
+          {PortfolioData.filter((item) => item.id === modalId).map((item) => {
+            //
+            return (
+              <div key={item.id} data-aos="fade">
+                <h2 className="heading mb-2">{item.type}</h2>
+                <div className="modal__details">
+                  {item.modalDetails.map((details, i) => {
+                    return (
+                      <div key={i} className="row open-sans-font">
+                        <div className="col-12 col-sm-6 mb-2">
+                          <i className="fa fa-file-text-o pr-2"></i>
+                          Project:{" "}
+                          <span className="ft-wt-600 uppercase">
+                            {details.project}
+                          </span>
+                        </div>
+                        <div className="col-12 col-sm-6 mb-2">
+                          <i className="fa fa-user-o pr-2"></i>
+                          About :{" "}
+                          <span className="ft-wt-600 uppercase">
+                            {details.client}
+                          </span>
+                        </div>
+                        <div className="col-12 col-sm-6 mb-2">
+                          <i className="fa fa-code pr-2"></i>
+                          Language :{" "}
+                          <span className="ft-wt-600 uppercase">
+                            {details.language}
+                          </span>
+                        </div>
+                        <div className="col-12 col-sm-6 mb-2">
+                          <i className="fa fa-external-link pr-2"></i>
+                          Preview :{" "}
+                          <a
+                            className="preview-link"
+                            target="_blank"
+                            rel="noopener noreferrer nofollow"
+                            href={details.link}
+                          >
+                            {details.preview}
+                          </a>
+                        </div>
                       </div>
-                      <div className="col-12 col-sm-6 mb-2">
-                        <i className="fa fa-user-o pr-2"></i>
-                        About :{" "}
-                        <span className="ft-wt-600 uppercase">
-                          {details.client}
-                        </span>
-                      </div>
-                      <div className="col-12 col-sm-6 mb-2">
-                        <i className="fa fa-code pr-2"></i>
-                        Language :{" "}
-                        <span className="ft-wt-600 uppercase">
-                          {details.language}
-                        </span>
-                      </div>
-                      <div className="col-12 col-sm-6 mb-2">
-                        <i className="fa fa-external-link pr-2"></i>
-                        Preview :{" "}
-                        <a
-                          className="preview-link"
-                          target="_blank"
-                          rel="noopener noreferrer nofollow"
-                          href={details.link}
-                        >
-                          {details.preview}
-                        </a>
-                      </div>
+                    );
+                  })}
+                </div>
+                <figure className="modal__img">
+                  <Slider {...settings}>
+                    <div>
+                      <Image src={img1} alt="portfolio project demo" />
                     </div>
-                  );
-                })}
-              </div>
-              <figure className="modal__img">
-                {/* <img src={item.image} alt="portfolio project demo" /> */}
-                <video
-                  id="video"
-                  className="responsive-video"
-                  controls
-                  poster={item.image}
-                >
-                  <source src={'/assets/img/portfolio/video.mp4'} type="video/mp4" />
-                  {/* <source src="img/" type="video/mp4" /> */}
-                </video>
-              </figure>
+                    <div>
+                      <Image src={img2} alt="portfolio project demo" />
+                    </div>
+                    <div>
+                      <Image src={img3} alt="portfolio project demo" />
+                    </div>
+                  </Slider>
+                </figure>
 
-              <button
-                className="close-modal"
-                onClick={() => setGetModal(false)}
-              >
-                <Image src={CloseImg} alt="portfolio project demo" />
-              </button>
-            </div>
-          );
-        })}
+                <button
+                  className="close-modal"
+                  onClick={() => setGetModal(false)}
+                >
+                  <Image src={CloseImg} alt="portfolio project demo" />
+                </button>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
   );
 };
 
